44ee06d3486a162dbd2022bc15786b8e266196f9
Espebaum/gyopark <vocops5000@gmail.com>
2023-11-15 22:51:11 +0900

    how to align board pretty?

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    // 로그인 버튼이 클릭되었을 때 수행할 동작을 작성합니다.
+    // 예를 들어, 입력된 사용자 정보를 가져와 서버에 전송하거나 다른 동작을 수행할 수 있습니다.
+    alert("로그인");
+});
